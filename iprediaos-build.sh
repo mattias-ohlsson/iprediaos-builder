893d3fa270c27496052785b8c5728e56a6980fe4
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-04-27 15:36:21 +0300

    Update iprediaos-build.sh

--- iprediaos-build.sh
+++ iprediaos-build.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+
+# We need this (uglu method)
+yum install -y git
+
+# Go home :)
+
+cd /root
+
+# Create the git folder
+mkdir git
+
+# Go git :)
+cd git
+
+# Clone repo (gaaa... repo exist?)
+git clone https://github.com/mattias-ohlsson/iprediaos-builder.git
+
+# Go to build scripts
+cd iprediaos-builder
+
+# Pull updates
+git pull
