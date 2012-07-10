21d7466d70013db815d85036a029fde3ee739491
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-07-10 14:26:48 +0300

    Update master

--- iprediaos-build.sh
+++ iprediaos-build.sh
@@ -36,34 +36,40 @@ cd iprediaos
 # Pull updates
 git pull
 
+# Create iso folder
+mkdir -p /root/iprediaos/iso/
+
+# Delete old isos
+find /root/iprediaos/iso/ -type f -mtime +3 -exec rm {} \;
+
 # make iso and rename
 echo "livecd-creator ..."
 
 make livecd-desktop-i686
 mv IprediaOS-*-i686-Live-Desktop.iso IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop.iso
+echo "Move and sync iso"
+mv *.iso /root/iprediaos/iso/
+/root/iprediaos/sync-files.sh
 
 make livecd-lxde-i686
 mv IprediaOS-*-i686-Live-LXDE.iso IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE.iso
+echo "Move and sync iso"
+mv *.iso /root/iprediaos/iso/
+/root/iprediaos/sync-files.sh
 
 make livecd-desktop-i686-sv_SE
 mv IprediaOS-*-i686-Live-Desktop-sv_SE.iso IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-sv_SE.iso
+echo "Move and sync iso"
+mv *.iso /root/iprediaos/iso/
+/root/iprediaos/sync-files.sh
 
 make livecd-lxde-i686-sv_SE
 mv IprediaOS-*-i686-Live-LXDE-sv_SE.iso IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-sv_SE.iso
+echo "Move and sync iso"
+mv *.iso /root/iprediaos/iso/
+/root/iprediaos/sync-files.sh
 
 echo "livecd-creator done"
-
-# Move isos
-mkdir -p /root/iprediaos/iso/
-mv *.iso /root/iprediaos/iso/
-
-# Delete old isos
-find /root/iprediaos/iso/ -type f -mtime +3 -exec rm {} \;
-
-# Sync files
-echo "sync files ..."
-/root/iprediaos/sync-files.sh
-echo "sync files done"
 
 # (disabled) Reboot to fix loopback device problems
 # echo "reboot ..."
