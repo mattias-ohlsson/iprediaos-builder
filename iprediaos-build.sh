e8e1b7f82b8e95624f636f10f6dc943c45e96ee6
Mattias Ohlsson <mattias.ohlsson@inprose.com>
2012-07-12 15:39:07 +0300

    Update master

--- iprediaos-build.sh
+++ iprediaos-build.sh
@@ -40,33 +40,57 @@ git pull
 mkdir -p /root/iprediaos/iso/
 
 # Delete old isos
-find /root/iprediaos/iso/ -type f -mtime +3 -exec rm {} \;
+find /root/iprediaos/iso/ -type f -mtime +1 -exec rm {} \;
 
 # make iso and rename
 echo "livecd-creator ..."
 
 make livecd-desktop-i686
 mv IprediaOS-*-i686-Live-Desktop.iso IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop.iso
+echo "Hash: SHA256\n\n" > IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-CHECKSUM
+sha256sum IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop.iso >> IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-CHECKSUM
 echo "Move and sync iso"
-mv *.iso /root/iprediaos/iso/
+mv *.iso *CHECKSUM /root/iprediaos/iso/
 /root/iprediaos/sync-files.sh
 
 make livecd-lxde-i686
 mv IprediaOS-*-i686-Live-LXDE.iso IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE.iso
+echo "Hash: SHA256\n\n" > IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-CHECKSUM
+sha256sum IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE.iso >> IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-CHECKSUM
 echo "Move and sync iso"
-mv *.iso /root/iprediaos/iso/
+mv *.iso *CHECKSUM /root/iprediaos/iso/
 /root/iprediaos/sync-files.sh
 
 make livecd-desktop-i686-sv_SE
 mv IprediaOS-*-i686-Live-Desktop-sv_SE.iso IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-sv_SE.iso
+echo "Hash: SHA256\n\n" > IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-sv_SE-CHECKSUM
+sha256sum IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-sv_SE.iso >> IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-sv_SE-CHECKSUM
 echo "Move and sync iso"
-mv *.iso /root/iprediaos/iso/
+mv *.iso *CHECKSUM /root/iprediaos/iso/
 /root/iprediaos/sync-files.sh
 
 make livecd-lxde-i686-sv_SE
 mv IprediaOS-*-i686-Live-LXDE-sv_SE.iso IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-sv_SE.iso
+echo "Hash: SHA256\n\n" > IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-sv_SE-CHECKSUM
+sha256sum IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-sv_SE.iso >> IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-sv_SE-CHECKSUM
 echo "Move and sync iso"
-mv *.iso /root/iprediaos/iso/
+mv *.iso *CHECKSUM /root/iprediaos/iso/
+/root/iprediaos/sync-files.sh
+
+make livecd-desktop-i686-ru_RU
+mv IprediaOS-*-i686-Live-Desktop-ru_RU.iso IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-ru_RU.iso
+echo "Hash: SHA256\n\n" > IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-ru_RU-CHECKSUM
+sha256sum IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-ru_RU.iso >> IprediaOS-$(date +%Y%m%d)-i686-Live-Desktop-ru_RU-CHECKSUM
+echo "Move and sync iso"
+mv *.iso *CHECKSUM /root/iprediaos/iso/
+/root/iprediaos/sync-files.sh
+
+make livecd-lxde-i686-ru_RU
+mv IprediaOS-*-i686-Live-LXDE-ru_RU.iso IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-ru_RU.iso
+echo "Hash: SHA256\n\n" > IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-ru_RU-CHECKSUM
+sha256sum IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-ru_RU.iso >> IprediaOS-$(date +%Y%m%d)-i686-Live-LXDE-ru_RU-CHECKSUM
+echo "Move and sync iso"
+mv *.iso *CHECKSUM /root/iprediaos/iso/
 /root/iprediaos/sync-files.sh
 
 echo "livecd-creator done"
